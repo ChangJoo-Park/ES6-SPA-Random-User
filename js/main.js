690784c9260228aa088010615985fdc3bef8ff0a
박새미 <sam@bagsaemiui-MacBook-Pro-2.local>
2017-10-13 11:23:10 +0900

    [update] 코드 리펙토링: main.js

--- js/main.js
+++ js/main.js
@@ -5,43 +5,32 @@
 * @Last Modified time: 2016-05-10 21:13:44
 */
 
-'use strict';
 import * as service from './contact-service-mock';
 import ContactDOM from './contact-dom';
 
-document.addEventListener('DOMContentLoaded', function () {
-  service.findAll().then((contacts) => {
-    loadContact(contacts);
-  });
-});
+const findId = id => document.getElementById(id);
+const loadContact = contacts => findId('contacts').innerHTML = contacts.reduce((p, contact) => p + new ContactDOM(contact).domObject, '');
+
+document.addEventListener('DOMContentLoaded', _ => service.findAll()
+  .then(contacts => loadContact(contacts)));
 
 //  Click Contact Reload button
-document.getElementById('reloadContacts').addEventListener('click', () => {
-  document.getElementById('flash').innerHTML = '';
-  document.getElementById('searchContactQuery').value = '';
-  service.findAll().then((contacts) => {
-    loadContact(contacts);
-  });
+findId('reloadContacts').addEventListener('click', _ => {
+  findId('flash').innerHTML = '';
+  findId('searchContactQuery').value = '';
+
+  service.findAll().then(contacts => loadContact(contacts));
 });
 
-document.getElementById('searchForm').addEventListener('submit', (event) => {
-  event.preventDefault();
-  document.getElementById('flash').innerHTML = '';
+findId('searchForm').addEventListener('submit', e => {
+  e.preventDefault();
+  findId('flash').innerHTML = '';
 
-  const queryText = document.getElementById('searchContactQuery');
-  service.findByName(queryText.value).then((contacts) => {
-    loadContact(contacts);
-  }).catch((error) => {
-    document.getElementById('flash').innerHTML = `<div class="alert alert-danger" role="alert">${error.message}</div>`;
-    queryText.value = "";
-  });
+  const queryText = findId('searchContactQuery');
+  service.findByName(queryText.value)
+    .then(contacts => loadContact(contacts))
+    .catch(({message}) => {
+      findId('flash').innerHTML = `<div class="alert alert-danger" role="alert">${message}</div>`;
+      queryText.value = "";
+    });
 });
-
-function loadContact(contacts) {
-  let html = '';
-  contacts.forEach((contact) => {
-    let contactDOM = new ContactDOM(contact);
-    html += contactDOM.domObject;
-  });
-  document.getElementById('contacts').innerHTML = html;
-};
